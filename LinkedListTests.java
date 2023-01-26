ee22c6b6d75d263abd3a49df5620a57905f3ba43
Luke8563 <122496000+Luke8563@users.noreply.github.com>
2023-01-25 16:37:04 -0800

    Fixed Linked List

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -1,5 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
 public class LinkedListTests {
-    
+    @Test
+    public void testAppend(){
+        LinkedList list = new LinkedList();
+        list.append(6);
+        list.append(7);
+        list.append(8);
+        list.append(9);
+
+        String s = "6 7 8 9 ";
+        assertEquals(s, list.toString());
+
+    }
+
+
+
 }
